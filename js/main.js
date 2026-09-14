b597144d5bae28d07a0addc5bc93992450c726ba
fgeroe <fabian.geroe@stud.hcw.ac.at>
2026-09-14 14:29:38 +0200

    Split script into modules

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import { loadBears } from "./bears.js";
+import { initCommentForm, initCommentToggle } from "./comments.js";
+import { initSearch } from "./search.js";
+
+initCommentToggle();
+initCommentForm();
+initSearch();
+loadBears();
